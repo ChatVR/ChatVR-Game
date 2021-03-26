f106c9a6634fbdd60abdbcc385590ceda8f69c15
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-03-26 19:26:14 -0400

    hand movement walking

--- Assets/Scripts/VRPlayerController.cs
+++ Assets/Scripts/VRPlayerController.cs
@@ -5,23 +5,68 @@ using UnityEngine;
 public class VRPlayerController : MonoBehaviour
 {
     [Range(0,10)]
-    public float playerSpeed;
+    public float playerSpeed = 0.2f;
 
+    [Range(0.1f, 1)]
+    public float moveSens = 0.2f;
+
+    public Collider col;
+    public Rigidbody rb;
+
+    /* Player Objects */
     public GameObject playerCamera;
+    public GameObject leftHand;
+    public GameObject rightHand;
+
+    /* Previous Locations */
+    private Vector3 prevPlayerLoc;
+    private Vector3 prevLeftHandLoc;
+    private Vector3 prevRightHandLoc;
 
     void Start()
     {
+        col = GetComponent<Collider>();
+        rb = GetComponent<Rigidbody>();
+
+        prevPlayerLoc = transform.position;
+        prevLeftHandLoc = leftHand.transform.position;
+        prevRightHandLoc = rightHand.transform.position;
     }
 
     void Update()
     {
-        Vector2 inp = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
 
-        if (inp == Vector2.zero) return;
+        //Vector2 inp = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
 
+        //if (inp == Vector2.zero) return;
+
+        Vector3 playerLoc = transform.position;
+        Vector3 leftHandLoc = leftHand.transform.position;
+        Vector3 rightHandLoc = rightHand.transform.position;
+
+        float playerDistance = Vector3.Distance(playerLoc, prevPlayerLoc);
+        float handSpeed = (Vector3.Distance(leftHandLoc, prevLeftHandLoc)-playerDistance)+
+            (Vector3.Distance(rightHandLoc, prevLeftHandLoc)-playerDistance);
+
+        prevPlayerLoc = playerLoc;
+        prevLeftHandLoc = leftHandLoc;
+        prevRightHandLoc = rightHandLoc;
+
+        Debug.Log(handSpeed);
+        if (handSpeed > moveSens)
+        {
+            Vector3 forward = Utils.toXZPlane(playerCamera.transform.forward).normalized;
+
+            rb.velocity = playerSpeed * forward;
+        }
+
+
+
+        /*
         Vector3 facingForward = Utils.toXZPlane(playerCamera.transform.forward).normalized;
         Vector3 facingSide = Utils.toXZPlane(playerCamera.transform.right).normalized;
         transform.Translate((facingForward*inp.y+facingSide*inp.x)*playerSpeed*Time.deltaTime);
+        */
     }
 
 }
